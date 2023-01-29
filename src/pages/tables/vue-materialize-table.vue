17cc49732c5c17f0a74ce9b6b5e963f671b70f59
jaybharadia <jay.p.bharadia@gmail.com>
2023-01-29 13:47:57 +0530

    WIP

---
+++ src/pages/tables/vue-materialize-table.vue
@@ -0,0 +1,72 @@
+<template>
+  <div>
+    <DataTable
+      title="Basic table"
+      :columns="tableColumns1"
+      :rows="tableRows1"
+    />
+  </div>
+</template>
+
+<script>
+import DataTable from "vue-materialize-datatable";
+
+export default {
+  components: {
+    DataTable,
+  },
+
+  data() {
+    return {
+      tableColumns1: [
+        {
+          label: "Character name",
+          field: "charName",
+          numeric: false,
+          html: false,
+        },
+        {
+          label: "First appearance",
+          field: "firstAppearance",
+          numeric: false,
+          html: false,
+        },
+        {
+          label: "Created by",
+          field: "createdBy",
+          numeric: false,
+          html: false,
+        },
+        {
+          label: "Voiced by",
+          field: "voicedBy",
+          numeric: false,
+          html: false,
+        },
+      ],
+      tableRows1: [
+        {
+          charName: "Abu",
+          firstAppearance: "Alladin (1992)",
+          createdBy: "Joe Grant",
+          voicedBy: "Frank Welker",
+        },
+        {
+          charName: "Magic Carpet",
+          firstAppearance: "Alladin (1992)",
+          createdBy: "Randy Cartwright",
+          voicedBy: "N/A",
+        },
+        {
+          charName: "The Sultan",
+          firstAppearance: "Alladin (1992)",
+          createdBy: "Navid Negahban",
+          voicedBy: "Douglas Seale",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
