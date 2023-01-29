8c14f57a2f4883037e6920ffc9e9275068444eb3
jaybharadia <jay.p.bharadia@gmail.com>
2023-01-29 14:01:58 +0530

    Learning done

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -1,11 +1,31 @@
 <template>
   <div>
     <h3>PROFILE COMPONENT</h3>
+
+    <div>
+      Item Prop
+      {{ value }}
+    </div>
+
+    <div>
+      BAG PROP
+      {{ bag }}
+    </div>
+
+    <button type="button" @click="$emit('input', 'hardar')">
+      Update item prop
+    </button>
+
+    <button @click="$emit('input:bag', 'large')">UPdate bag prop</button>
   </div>
 </template>
 
 <script>
 export default {
+  props: {
+    value: [String, Array],
+    bag: String,
+  },
   created() {
     console.log("inside created hook");
   },
--- src/pages/about.vue
+++ src/pages/about.vue
@@ -2,12 +2,18 @@
   <div class="about">
     <h1>This is an about page</h1>
 
-    <Profile />
+    <Profile v-model="item" v-model:bag="bag" />
   </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      item: "cocunt",
+      bag: "small",
+    };
+  },
   components: {
     Profile: require("@/components/Profile.vue").default,
   },
